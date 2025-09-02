6fb3680faa168a921c66e70d6e510f4d35eb47b3
Ankit-GLB <ankitraj.ms11@gmail.com>
2025-09-02 19:54:08 +0530

    debug

--- script.sh
+++ script.sh
@@ -6,14 +6,13 @@ echo "running script.sh"
 
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 echo "1"
-# sudo docker pull "${REPO_NAME}":"${REPO_TAG}"
-sudo docker pull alpine/figlet
+sudo docker pull ""${DOCKER_USERNAME}"/${REPO_NAME}":"${REPO_TAG}"
 echo "2"
-# sudo docker stop -t 30 "${APP_NAME}:${REPO_TAG}" 2>/dev/null # wait for 30 sec before sending SIGKILL signal
-# echo "3"
-# sudo docker rm "${APP_NAME}:${REPO_TAG}" 2>/dev/null
-# echo "4"
-# sudo docker run --name "${APP_NAME}:${REPO_TAG}" -d -p 443:"${APP_PORT}" -p 80:"${APP_PORT}" "${REPO_NAME}":"${REPO_TAG}"
-# echo "5"
-# echo "exiting script.sh"
-# echo "6"
+sudo docker stop -t 30 "${APP_NAME}:${REPO_TAG}" 2>/dev/null # wait for 30 sec before sending SIGKILL signal
+echo "3"
+sudo docker rm "${APP_NAME}:${REPO_TAG}" 2>/dev/null
+echo "4"
+sudo docker run --name "${APP_NAME}:${REPO_TAG}" -d -p 443:"${APP_PORT}" -p 80:"${APP_PORT}" "${REPO_NAME}":"${REPO_TAG}"
+echo "5"
+echo "exiting script.sh"
+echo "6"
