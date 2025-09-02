0fc0431901893c8c7f24fa8fca322f6ee0121c07
Ankit-GLB <ankitraj.ms11@gmail.com>
2025-09-02 19:42:39 +0530

    Added sudo when running docker commands

--- script.sh
+++ script.sh
@@ -6,12 +6,12 @@ echo "running script.sh"
 
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
-docker pull "${REPO_NAME}":"${REPO_TAG}"
+sudo docker pull "${REPO_NAME}":"${REPO_TAG}"
 
-docker stop -t 30 "${APP_NAME}:${REPO_TAG}" 2>/dev/null # wait for 30 sec before sending SIGKILL signal
+sudo docker stop -t 30 "${APP_NAME}:${REPO_TAG}" 2>/dev/null # wait for 30 sec before sending SIGKILL signal
 
-docker rm "${APP_NAME}:${REPO_TAG}" 2>/dev/null
+sudo docker rm "${APP_NAME}:${REPO_TAG}" 2>/dev/null
 
-docker run --name "${APP_NAME}:${REPO_TAG}" -d -p 443:"${APP_PORT}" -p 80:"${APP_PORT}" "${REPO_NAME}":"${REPO_TAG}"
+sudo docker run --name "${APP_NAME}:${REPO_TAG}" -d -p 443:"${APP_PORT}" -p 80:"${APP_PORT}" "${REPO_NAME}":"${REPO_TAG}"
         
 echo "exiting script.sh"
